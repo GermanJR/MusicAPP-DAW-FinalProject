cea686e023781c11566deb4c6b4b8352592db5ff
GermanJR <wambows@gmail.com>
2024-04-15 16:02:11 +0200

    Update en la página de perfil

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -35,13 +35,13 @@ export default defineComponent({
   <h1 class="col-12">My profile</h1>
   <div class="container text-center">
     <div class="row">
-      <h2 class="col-12 col-md-6 text-center">{{ user.display_name }}</h2>
-      <img id="pfp" :src="profile" alt="Profile picture" class="col-12 col-md-6">
+      <h2 style="padding-left: 220px" class="col-12 col-md-10 text-center"><b>{{ user.display_name }}</b></h2>
+      <img id="pfp" :src="profile" alt="Profile picture" class="col-12 col-md-2">
       <h2 class="col-12">Country: {{ user.country }}</h2>
       <h2 class="col-12">Followers: {{ user.followers.total }}</h2>
       <div class="row">
-        <h2 class="col-12 ">Account type: {{ user.product }}</h2>
-        <button id="login_button" type="button" class="col-12 text-start" @click="goToSpotifyProfile">
+        <h2 class="col-12">Account type: {{ user.product }}</h2>
+        <button id="go_to_button" type="button" class="col-12 text-start" @click="goToSpotifyProfile">
           <link-icon></link-icon>
           View on Spotify
         </button>
@@ -56,9 +56,10 @@ export default defineComponent({
   border-radius: 20px;
   border: solid 2px #000;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding: 20px;
 }
 
-#login_button {
+#go_to_button {
   height: 40px;
   width: 200px;
   background-color: #1ED760;
@@ -67,5 +68,20 @@ export default defineComponent({
   color: #000;
   font-weight: bold;
   padding-top: 2px;
+  margin-left: auto !important;
+  margin-right: auto;
+  align-content: center !important;
+  margin-top: 20px;
+}
+
+#pfp {
+  height: 150px;
+  width: 150px;
+  border: solid 2px #000;
+  border-radius: 1000px;
+  padding: 0 !important;
+  display: block;
+  margin-left: auto;
+  margin-right: 20px;
 }
 </style>
