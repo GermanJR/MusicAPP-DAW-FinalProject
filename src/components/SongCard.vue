845b90dd594034ce93355a0ce2f1ab18954c4fe8
GermanJR <wambows@gmail.com>
2024-04-17 15:12:14 +0200

    Implementando funcionalidades en el dashboard

---
+++ src/components/SongCard.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "SongCard",
+  props: {
+    songReceived: {
+      type: Object,
+      required: true,
+    }
+  },
+
+  methods: {
+    getArtists() {
+      let totalArtists = "";
+      this.songReceived.artists.forEach((artist, index) => {
+        totalArtists += artist;
+        if (index !== this.songReceived.artists.length - 1) {
+          totalArtists += ",";
+        }
+      });
+      return totalArtists;
+    }
+  }
+}
+</script>
+
+<template>
+<div class="col-12">
+  <h4>{{songReceived.name}}</h4>
+  <h5>{{getArtists()}}</h5>
+</div>
+</template>
+
+<style scoped>
+div {
+  background-color: #191414;
+}
+</style>
--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -8,8 +8,27 @@ export default defineComponent({
   computed: {
     ...mapState(userStore, {
       user: "currentUser",
-    })
+    }),
   },
+
+  data() {
+    return {
+      showRecommendedSongs: false,
+      recommendedSongsValue: 5,
+      recommendedSongs: {},
+      periodSelectValue: "medium_term",
+    }
+  },
+
+  methods: {
+    toggleRecommendedSongs(){
+      this.showRecommendedSongs = !this.showRecommendedSongs;
+    },
+
+    handleRecommendedSongsCall(){
+
+    }
+  }
 });
 </script>
 
@@ -19,11 +38,53 @@ export default defineComponent({
       <h1 class="col-12">DashBoard</h1>
       <h3 class="col-12">Welcome again <b>{{ user?.display_name }}</b></h3>
     </div>
+    <div class="row">
+      <h3 class="col-12">Here are some interesting features for you:</h3>
+      <h4 class="col-12">Discover your top songs</h4>
+      <button v-if="!showRecommendedSongs" type="button" @click="toggleRecommendedSongs" id="openButton">Try it!</button>
+      <div v-if="showRecommendedSongs" class="row">
+        <p class="col-12 col-sm-6">Number of songs:</p>
+        <input type="range" min="1" max="10" v-model="recommendedSongsValue" class="slider col-11 col-sm-5" id="songsRange" ><span class="col-1">{{recommendedSongsValue}}</span>
+        <label for="period" class="col-12 col-sm-6">Period of time:</label>
+
+        <select name="period" id="period" class="col-12 col-sm-6" v-model="periodSelectValue">
+          <option value="short_term">Short</option>
+          <option value="medium_term">Medium</option>
+          <option value="long_term">Long</option>
+        </select>
+
+        <button v-if="showRecommendedSongs" type="button" @click="toggleRecommendedSongs" id="closeButton" class="col-12">Close</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 b {
   color: #35c500;
+}
+
+#openButton {
+  height: 40px;
+  width: 200px;
+  background-color: #1ED760;
+  border: solid 2px #000;
+  border-radius: 20px;
+  color: #000;
+  font-weight: bold;
+  padding-top: 2px;
+  margin-top: 25px;
+}
+
+#closeButton {
+  height: 40px;
+  width: 200px;
+  background-color: #ff4f3f;
+  border: solid 2px #000;
+  border-radius: 20px;
+  color: #000;
+  font-weight: bold;
+  padding-top: 2px;
+  margin-top: 25px;
 }
 </style>
